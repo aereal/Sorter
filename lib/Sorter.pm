aea95308157eb6edf7d235ec6e2cd672af019e7f
aereal <aereal@kerare.org>
2012-03-19 17:48:09 +0900

    Indent by 4 spaces

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -3,30 +3,30 @@ use strict;
 use warnings;
 
 sub new {
-  my $class = shift;
-  my $self = {};
-  bless $self, $class;
-  $self->initialize;
-  return $self;
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    $self->initialize;
+    return $self;
 }
 
 sub initialize {
-  my $self = shift;
-  $self->set_values;
+    my $self = shift;
+    $self->set_values;
 }
 
 sub sort {
-  #
+    #
 }
 
 sub get_values {
-  my $self = shift;
-  return @{ $self->{values} };
+    my $self = shift;
+    return @{ $self->{values} };
 }
 
 sub set_values {
-  my ($self, @args) = @_;
-  $self->{values} = \@args;
+    my ($self, @args) = @_;
+    $self->{values} = \@args;
 }
 
 1;
